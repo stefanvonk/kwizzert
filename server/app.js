1bccfcc5e94ce188ff62a639e5e0b734e14c5049
Unknown <s1084294@student.hsleiden.nl>
2017-04-03 14:50:24 +0200

    socketserver gestart

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 var express     = require('express');
 // var bodyParser  = require('body-parser');
 var path        = require('path');
+var ws                 = require('ws');
+var http               = require('http');
+var theHttpServer      = http.createServer();
+var theWebSocketServer = new ws.Server({
+    server: theHttpServer
+});
+
+var startkwizFile = require('./messages/startkwiz');
 
 var app = express();
 // app.use(bodyParser.json());
@@ -18,6 +26,56 @@ app.use('/scorebord', express.static(path.join(__dirname, '../Clients/scorebord/
 //     res.sendFile(path.join(__dirname + '/Clients/kwizmeestert/build/index.html'));
 // })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+theHttpServer.on('request', app);
+theHttpServer.listen( 3000,
+    function() {
+        console.log("The Server is lisening on port 3000.")
+    });
+
+theWebSocketServer.on('connection', function connection(websocket) {
+    console.log("CONNECTION CREATED");
+    websocket.on('message', function incoming(message) {
+        console.log("Het werkt!!");
+        switch(message) {
+            case "aanmeldenteam":
+                console.log("aanmeldenteam");
+                break;
+            case "ontvangstantwoord":
+                console.log("ontvangstantwoord");
+                break;
+            case "startkwizavond":
+                console.log("startkwizavond");
+                break;
+            case "teamgeaccepteerd":
+                console.log("teamgeaccepteerd");
+                break;
+            case "startkwiz":
+                startkwizFile();
+                break;
+            case "startronde":
+                console.log("startronde");
+                break;
+            case "stopkwiz":
+                console.log("stopkwiz");
+                break;
+            case "startvraag":
+                console.log("startvraag");
+                break;
+            case "stopvraag":
+                console.log("stopvraag");
+                break;
+            case "antwoordgecontroleerd":
+                console.log("antwoordgecontroleerd");
+                break;
+            case "volgende":
+                console.log("volgende");
+                break;
+        }
+    });
+    websocket.on('close', function() {
+        console.log('CONNECTION FOR ' + websocket.type + " CLOSED.");
+        if(websocket.timeoutObject) {
+            clearTimeout(websocket.timeoutObject);
+        }
+    });
+});
