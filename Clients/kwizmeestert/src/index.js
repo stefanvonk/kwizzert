b148874f5fabfedc25280ba0acc7c1d4880eac92
Unknown <s1084294@student.hsleiden.nl>
2017-04-04 14:46:26 +0200

    Clientside routing toegevoegd, werkt niet..

--- Clients/kwizmeestert/src/index.js
+++ Clients/kwizmeestert/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import kwizstarten from './modules/kwizstarten'
+import teamsaccepteren from './modules/teamsaccepteren'
+import { Router, Route, browserHistory } from 'react-router'
 
 ReactDOM.render(
-  <App />,
+    <App />,
+  // <Router history={browserHistory}>
+  //   <Route path="/" component={App}/>
+  //   <Route path="/kwizstarten" component={kwizstarten}/>
+  //   <Route path="/teamsaccepteren" component={teamsaccepteren}/>
+  // </Router>,
   document.getElementById('root')
 );
