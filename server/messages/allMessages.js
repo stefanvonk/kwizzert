9e0d40eb414ea46cc96b968d7558ad23bb04151c
Christiaan ten Voorde <CI.TenVoorde@student.han.nl>
2017-04-05 15:45:49 +0200

    Voeg de messages samen. Mooier bij importen.

---
+++ server/messages/allMessages.js
@@ -0,0 +1,9 @@
+var startkwiz = require('./startkwiz');
+var startkwizavond = require('./startkwizavond');
+
+const allMessages = {
+    startkwiz,
+    startkwizavond
+};
+
+export default allMessages
