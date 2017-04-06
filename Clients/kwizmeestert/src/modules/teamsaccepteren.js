175cf1df483d65d0f1b6a7a353429359984fee5d
Unknown <s1084294@student.hsleiden.nl>
2017-04-06 23:00:47 +0200

    Gewerkt aan state in teamsaccepteren

--- Clients/kwizmeestert/src/modules/teamsaccepteren.js
+++ Clients/kwizmeestert/src/modules/teamsaccepteren.js
@@ -3,54 +3,73 @@ import './../App.css';
 import { Button, FormControl } from 'react-bootstrap';
 import { browserHistory } from 'react-router';
 
-var socketKwizmeestert = new WebSocket("ws:localhost:3000/");
-socketKwizmeestert.onopen = function (event) {};
-
 var teams = {
-    teamnamen: ["team1"]
-};
-
-socketKwizmeestert.onmessage = function incoming(message) {
-    var data = JSON.parse(message.data);
-    if(data.type === "teamaangemeld") {
-        teams.teamnamen.push(data.teamnaam);
-    }
+    teamnamen: ["team1", "team2", "team3"]
 };
 
 class Teamsaccepteren extends React.Component {
     constructor(props) {
         super(props);
-        console.log(teams);
-        this.setState = {
-            teams
-        };
-        console.log(this.state);
+        this.state = {teams};
     }
 
+    componentDidMount() {
+        var socketKwizmeestert = new WebSocket("ws:localhost:3000/");
+        socketKwizmeestert.onopen = function (event) {};
 
+        socketKwizmeestert.onmessage = function incoming(message) {
+            var data = JSON.parse(message.data);
+            if(data.type === "teamaangemeld") {
+                teams.teamnamen.push(data.teamnaam);
+            }
+        };
+        this.setState(teams);
+    }
 
-    handleClick() {
+    componentWillUnmount() {
+
+    }
+
+    startButton() {
+        teams.teamnamen.push("team2");
+        this.setState(teams.teamnamen);
+    }
+
+    teamAccepteren(){
+
         // socketKwizmeestert.send(JSON.stringify(data));
-        // console.log("het werkt");
-        // teams.teamnamen.push("team2");
-        // this.setState(function() {
-        //     return {
-        //         teams
-        //     };
-        // });
-        // console.log(this.state);
+    }
+
+    teamWeigeren(){
+
+        // socketKwizmeestert.send(JSON.stringify(data));
     }
 
     render() {
+        var teamtoevoegen = function() {
+            var button1 = JSON.stringify(<Button bsStyle="Success" onClick={(e) => this.teamAccepteren(e)}>Starten</Button>);
+            var button2 = JSON.stringify(<Button bsStyle="Danger" onClick={(e) => this.teamWeigeren(e)}>Starten</Button>);
+            return "<div><h2>" + "</h2>" + button1 + button2 + "</div>";
+        };
+
+        var teamToevoegen = function() {
+            console.log("test1");
+            this.state.teams.teamnamen.forEach(function (teams123) {
+                console.log("test1");
+                return "<h2>" + teams123 + "</h2>";
+            });
+            console.log("test3");
+        };
+
         return (
             <div className="App">
                 <h1>Teams</h1>
-                {this.state}
-                <Button bsStyle="primary" onClick={(e) => this.handleClick(e)}>Starten</Button>
+                {this.state.teams.teamnamen}
+                {teamToevoegen}
+                <Button bsStyle="primary" onClick={(e) => this.startButton(e)}>Starten</Button>
             </div>
         );
     }
 }
 
 module.exports = Teamsaccepteren;
-
