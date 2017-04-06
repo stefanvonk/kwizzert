51b37c0e90279cdca2b38c072f5b754f4ae73649
Christiaan ten Voorde <CI.TenVoorde@student.han.nl>
2017-04-06 20:01:35 +0200

    Begin gemaakt voor het aanmelden van team.

---
+++ Clients/team/src/modules/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            teamnaam: ""
+        }
+    }
+
+  render() {
+    return (
+        <div>
+            {this.props.children}
+        </div>
+    );
+  }
+}
+
+export default App;
