1b23a0384b18de6a5279874735e44f9484743b68
Christiaan ten Voorde <CI.TenVoorde@student.han.nl>
2017-04-07 15:58:51 +0200

    Start Scoreboard app.

--- Clients/scorebord/src/index.js
+++ Clients/scorebord/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './modules/App';
 import './index.css';
+import InlogScherm from './modules/inlogScherm'
+import VoorafKwiz from './modules/voorafKwiz'
+import AchterafKwiz from './modules/achterafKwiz'
+import BeoordelingVraag from './modules/beoordelingVraag'
+import ActieveVraag from './modules/actieveVraag'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+    <Router history={browserHistory}>
+        <Route path="/scorebord" component={App}>
+            <IndexRoute component={InlogScherm}/>
+            <Route path="/scorebord/voorafKwiz" component={VoorafKwiz}/>
+            <Route path="/scorebord/achterafKwiz" component={AchterafKwiz}/>
+            <Route path="/scorebord/beoordelingVraag" component={BeoordelingVraag}/>
+            <Route path="/scorebord/actieveVraag" component={ActieveVraag}/>
+        </Route>
+    </Router>,
+    document.getElementById('root')
 );
