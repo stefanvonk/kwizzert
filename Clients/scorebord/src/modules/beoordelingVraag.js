9edf5f4504b119047977c92878567c470a8dada5
Christiaan ten Voorde <CI.TenVoorde@student.han.nl>
2017-04-10 20:06:27 +0200

    Tabel voor beoordelingen.

--- Clients/scorebord/src/modules/beoordelingVraag.js
+++ Clients/scorebord/src/modules/beoordelingVraag.js
@@ -12,25 +12,33 @@ class BeoordelingVraag extends React.Component {
                     Vraagnr: {this.props.vraagnummer}
                 </div>
                 <table>
-                    <th>
-                        <td>Teamnaam</td>
-                        <td>Antwoord</td>
-                        <td></td>
-                        <td>Rondepunten</td>
-                        <td>Vragen goed</td>
-                    </th>
-                    {this.props.teams.forEach((team) =>
-                        <tr>
-                            <td>{team.teamnaam}</td>
-                            <td>{team.antwoord}</td>
-                            {/*<td>{team.antwoordGoed}</td>*/}
-                            <td>{team.rondepunten}</td>
-                            <td>{team.vragengoed}</td>
-                        </tr>
+                    <tr>
+                        <th>Teamnaam</th>
+                        <th>Antwoord</th>
+                        <th></th>
+                        <th>Rondepunten</th>
+                        <th>Vragen goed</th>
+                    </tr>
+                    {this.props.teams.map((team) =>
+                        <BeoordelingTeam team={team}/>
                     )}
                 </table>
             </div>
         );
+    }
+}
+
+class BeoordelingTeam extends React.Component {
+    render() {
+        return(
+            <tr>
+                <td>{this.props.team.teamnaam}</td>
+                <td>{this.props.team.antwoord}</td>
+                {/*<td>{this.props.team.antwoordGoed}</td>*/}
+                <td>{this.props.team.rondepunten}</td>
+                <td>{this.props.team.vragengoed}</td>
+            </tr>
+        )
     }
 }
 
