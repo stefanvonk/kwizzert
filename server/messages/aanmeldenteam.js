7d1786409cfe21d868f71b9299fd678ff8f6f4d6
Christiaan ten Voorde <CI.TenVoorde@student.han.nl>
2017-04-11 13:45:51 +0200

    Fix uniek teamnaam

--- server/messages/aanmeldenteam.js
+++ server/messages/aanmeldenteam.js
@@ -2,17 +2,26 @@ var functie = function(code, teamnaam, session, websocket){
     let returnSocket;
     const kwiz = session.kwizzen.find(x => x.code === code);
     if(kwiz){
-        kwiz.teams.push({
-            teamSocket: websocket,
-            teamnaam:teamnaam,
-            rondepunten: 0,
-            vragenGoed: 0
-        });
+        const teamMetZelfdeNaam = kwiz.teams.find(x => x.teamnaam === teamnaam);
+        if(!teamMetZelfdeNaam) {
+            kwiz.teams.push({
+                teamSocket: websocket,
+                teamnaam: teamnaam,
+                rondepunten: 0,
+                vragenGoed: 0
+            });
 
-        returnSocket = kwiz.kwizmeestertSocket;
-        data = {
-            Type: "teamaangemeld",
-            teamnaam: teamnaam
+            returnSocket = kwiz.kwizmeestertSocket;
+            data = {
+                Type: "teamaangemeld",
+                teamnaam: teamnaam
+            }
+        } else{
+            returnSocket = websocket;
+            data = {
+                Type: "teamgeaccepteerd",
+                melding: "Teamnaam al bezet."
+            }
         }
     } else{
         returnSocket = websocket;
